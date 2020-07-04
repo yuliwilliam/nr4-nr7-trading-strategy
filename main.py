8e3ef9e44228d13096d6316c879571058e8272a3
Lizhi Yu <leedsyu@yahoo.com>
2020-07-03 20:57:47 -0400

    refactor

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def get_everyday_data(data):
     data_list = []
     # data_size = data.shape[0]
     # smaller data size for testing purpose
-    data_size = int(data.shape[0] * 0.01)
+    data_size = int(data.shape[0] * 0.1)
     for i in range(data_size):
         curr_day_ticker = data['<TICKER>'][i]
         curr_day_date = data['<DTYYYYMMDD>'][i]
@@ -38,7 +38,33 @@ def get_everyday_data(data):
     return data_list
 
 
+def get_nr4_days(everyday_data, pip):
+    buy_sell_prices = []
+    # exclude last day
+    for i in range(3, len(everyday_data) - 1):
+        first_price_range = everyday_data[i - 3]['HIGH'] - everyday_data[i - 3]['LOW']
+        second_price_range = everyday_data[i - 2]['HIGH'] - everyday_data[i - 2]['LOW']
+        third_price_range = everyday_data[i - 1]['HIGH'] - everyday_data[i - 1]['LOW']
+        fourth_price_range = everyday_data[i]['HIGH'] - everyday_data[i]['LOW']
+        if min(first_price_range, second_price_range, third_price_range, fourth_price_range) == fourth_price_range \
+                and everyday_data[i]['LOW'] >= everyday_data[i - 1]['LOW'] \
+                and everyday_data[i]['HIGH'] <= everyday_data[i - 1]['HIGH']:
+            transaction_date = everyday_data[i + 1]['DATE']
+            buy_price = everyday_data[i]['HIGH'] + everyday_data[i]['HIGH'] * pip
+            short_price = everyday_data[i]['LOW'] - everyday_data[i]['LOW'] * pip
+
+            buy_sell_price = {'DATE': transaction_date, 'BUY': buy_price, 'SHORT': short_price}
+            buy_sell_prices.append(buy_sell_price)
+            print("NR4 day, data: {}, high: {}, low: {}, price range: {}".format(everyday_data[i]['DATE'],
+                                                                                 everyday_data[i]['HIGH'],
+                                                                                 everyday_data[i]['LOW'],
+                                                                                 fourth_price_range))
+    return buy_sell_prices
+
+
 def test_result(data, buy_sell_prices, cash, stop_loss_pip, stop_profit_pip):
+    start_cash = cash
+    print("started testing with {} cash".format(start_cash))
     profit_loss_data = {'DATE': [], 'PROFIT LOSS PERCENTAGE': [], 'PROFIT LOSS AMOUNT': []}
     for buy_sell_price in buy_sell_prices:
         initial_cash = cash
@@ -142,38 +168,21 @@ def test_result(data, buy_sell_prices, cash, stop_loss_pip, stop_profit_pip):
     plt.suptitle("PROFIT LOSS AMOUNT vs DATE BAR GRAPH")
     plt.savefig("./graph/PROFIT LOSS AMOUNT vs DATE BAR GRAPH.png")
     plt.show()
+    print("started testing with {} cash".format(start_cash))
+    print("ended testing with {} cash".format(cash))
+    print("profit/loss amount: {}, profit/loss rate: {}".format(cash - start_cash, (cash - start_cash) / start_cash))
+
     return cash
 
 
 if __name__ == '__main__':
-    data_file_path = "./data/USD/USDMXN-1M-2008.1.1-2020.5.22.csv"
+    data_file_path = "./data/USD/USDJPY-1M-2004.1.1-2020.3.31.csv"
     cash = 10000
     pip = 0.0001
-    stop_loss_pip = 10 * pip
-    stop_profit_pip = 40 * pip
+    stop_loss_pip = 100 * pip
+    stop_profit_pip = 400 * pip
+
     data = load_data(data_file_path)
     everyday_data = get_everyday_data(data)
-    buy_sell_prices = []
-
-    # exclude last day
-    for i in range(3, len(everyday_data) - 1):
-        first_price_range = everyday_data[i - 3]['HIGH'] - everyday_data[i - 3]['LOW']
-        second_price_range = everyday_data[i - 2]['HIGH'] - everyday_data[i - 2]['LOW']
-        third_price_range = everyday_data[i - 1]['HIGH'] - everyday_data[i - 1]['LOW']
-        fourth_price_range = everyday_data[i]['HIGH'] - everyday_data[i]['LOW']
-        if min(first_price_range, second_price_range, third_price_range, fourth_price_range) == fourth_price_range \
-                and everyday_data[i]['LOW'] >= everyday_data[i - 1]['LOW'] \
-                and everyday_data[i]['HIGH'] <= everyday_data[i - 1]['HIGH']:
-            transaction_date = everyday_data[i + 1]['DATE']
-            buy_price = everyday_data[i]['HIGH'] + everyday_data[i]['HIGH'] * pip
-            short_price = everyday_data[i]['LOW'] - everyday_data[i]['LOW'] * pip
-
-            buy_sell_price = {'DATE': transaction_date, 'BUY': buy_price, 'SHORT': short_price}
-            buy_sell_prices.append(buy_sell_price)
-            print("NR4 day, data: {}, high: {}, low: {}, price range: {}".format(everyday_data[i]['DATE'],
-                                                                                 everyday_data[i]['HIGH'],
-                                                                                 everyday_data[i]['LOW'],
-                                                                                 fourth_price_range))
-    print("started testing with {} cash".format(cash))
+    buy_sell_prices = get_nr4_days(everyday_data, pip)
     cash = test_result(data, buy_sell_prices, cash, stop_loss_pip, stop_profit_pip)
-    print("ended testing with {} cash".format(cash))
